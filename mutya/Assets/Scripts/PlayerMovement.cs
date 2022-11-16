675aaa30007c9e452844ff851745602b2a450b89
feilkinez <75008462+feilkinez@users.noreply.github.com>
2022-11-17 01:22:53 +0800

    Cutscene + Disable movement for Menu

--- mutya/Assets/Scripts/PlayerMovement.cs
+++ mutya/Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed;
 
     public Rigidbody2D rb;
     public Animator animator;
 
     Vector2 movement;
+
+    public float timeRemain = 39;
+    public bool canMove = false;
 
     // Update is called once per frame
     void Update()
@@ -17,13 +20,26 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (canMove)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+        
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (timeRemain > 0)
+        {
+            timeRemain -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            canMove = true;
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        
     }
 }
