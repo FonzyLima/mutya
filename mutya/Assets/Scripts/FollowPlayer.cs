4edd7dac55d1aafb326c1173080c24ed0bbf4e16
feilkinez <75008462+feilkinez@users.noreply.github.com>
2022-11-21 02:14:30 +0800

    Diwata movement + Mutya Title (rough draft)

---
+++ mutya/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    public float timeRemain = 39;
+    public bool canMove = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
+        if (canMove)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+    }
+
+    // Executes on a fixed time
+    void FixedUpdate()
+    {
+        if (timeRemain > 0)
+        {
+            timeRemain -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            canMove = true;
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
