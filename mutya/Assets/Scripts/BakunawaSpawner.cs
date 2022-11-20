33a53eed381c20eca502d14202724f613baef643
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2022-11-20 23:07:23 +0800

    bakunawa flies

--- mutya/Assets/Scripts/BakunawaSpawner.cs
+++ mutya/Assets/Scripts/BakunawaSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BakunawaSpawner : MonoBehaviour
 {
     public GameObject objectToSpawn;
-    
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,10 @@ public class BakunawaSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y >10){
+            transform.position = new Vector3(Random.Range(5,24),-12,transform.position.z);
+        }
+        transform.Translate(Vector2.up*speed*Time.deltaTime);
+
     }
 }
