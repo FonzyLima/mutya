0038d1047244c58efc1f41f52e23bbda956f7d7c
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2022-11-20 23:43:01 +0800

    flies with cam

--- mutya/Assets/Scripts/BakunawaSpawner.cs
+++ mutya/Assets/Scripts/BakunawaSpawner.cs
@@ -6,17 +6,20 @@ public class BakunawaSpawner : MonoBehaviour
 {
     public GameObject objectToSpawn;
     public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y >10){
-            transform.position = new Vector3(Random.Range(5,24),-12,transform.position.z);
+        
+        if(transform.position.y > GameObject.FindGameObjectWithTag("MainCamera").transform.position.y+10){
+            transform.position = new Vector3(Random.Range(GameObject.FindGameObjectWithTag("MainCamera").transform.position.x-10,GameObject.FindGameObjectWithTag("MainCamera").transform.position.x+10),GameObject.FindGameObjectWithTag("MainCamera").transform.position.y-12,transform.position.z);
         }
         transform.Translate(Vector2.up*speed*Time.deltaTime);
 
