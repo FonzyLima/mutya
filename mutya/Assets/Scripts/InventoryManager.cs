ceae7ec924c07cf22e155363ab61f4906f18acf0
azrielortega21 <azriel_ortega@dlsu.edu.ph>
2022-11-23 12:55:43 -0400

    added pick up item with, items are hidden, text not yet added

---
+++ mutya/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public float tikbalang_item; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        tikbalang_item = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ mutya/Assets/Scripts/ItemScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemScript : MonoBehaviour
+{
+    private InventoryManager invManager;
+    private bool pickUpAllowed;
+    private bool isDiscovered;
+    private SpriteRenderer itemSprite;
+
+
+    [SerializeField] private Text pickUpText;
+
+    private void Start(){
+        //pickUpText.gameObject.SetActive(false);
+        itemSprite = GetComponent<SpriteRenderer>();
+        invManager = GameObject.Find("Player").GetComponent<InventoryManager>();
+        gameObject.GetComponent<Renderer>().enabled = false;
+    }
+
+    private void Update(){
+        if (isDiscovered && pickUpAllowed && Input.GetKeyDown(KeyCode.F)){
+            PickUp();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.tag == "Player"){
+
+            if (!isDiscovered){
+                gameObject.GetComponent<Renderer>().enabled = true;
+                isDiscovered = true;
+                pickUpAllowed = true;
+                //pickUpText.gameObject.SetActive(true);
+            }
+            else
+                pickUpAllowed = true;
+                //pickUpText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        if (collision.tag == "Player"){
+                pickUpAllowed = false;
+                pickUpText.gameObject.SetActive(false);
+        }
+    }
+
+    private void PickUp(){
+        
+        invManager.tikbalang_item += 1f;
+        Destroy(gameObject);
+    }
+}
