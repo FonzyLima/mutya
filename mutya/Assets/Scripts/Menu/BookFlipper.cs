8e6231c019b79c44db9b59f77d8bf6e54ea819ee
feilkinez <75008462+feilkinez@users.noreply.github.com>
2022-11-26 00:59:07 +0800

    Book animation with text works

--- mutya/Assets/Scripts/Menu/BookFlipper.cs
+++ mutya/Assets/Scripts/Menu/BookFlipper.cs
@@ -1,25 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BookFlipper : MonoBehaviour
 {
     public Animator animator;
-    public int pageCounter = 1;
+    public int pageCounter = 0;
+
+    public TextMeshProUGUI monsterName;
+    public TextMeshProUGUI monsterDesc;
+
+    public string[] monsters;
+    public string[] descs;
+
+    void Start()
+    {
+        monsterName.text = string.Empty;
+        monsterDesc.text = string.Empty;
+        SpawnText();
+    }
+
+    void SpawnText()
+    {   
+        monsterName.text += monsters[pageCounter];
+        monsterDesc.text += descs[pageCounter];
+    }
+
+    void RemoveText()
+    {
+        monsterName.text = string.Empty;
+        monsterDesc.text = string.Empty;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) && pageCounter < 10)
+        if (Input.GetKeyDown(KeyCode.RightArrow) && pageCounter < 9)
         {
+            RemoveText();
             animator.Play("Base Layer.FlipNext");
             pageCounter++;
+            SpawnText();
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && pageCounter > 1)
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && pageCounter > 0)
         {
+            RemoveText();
             animator.Play("Base Layer.FlipPrev");
             pageCounter--;
+            SpawnText();
         }
     }
 }
